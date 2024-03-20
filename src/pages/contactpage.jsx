11d933e8edab367595e4b1936402f621fa60fd7f
THGIE EIGHT <zzzzzza430@gmail.com>
2024-03-20 17:45:04 +0545

    'first'

--- src/pages/contactpage.jsx
+++ src/pages/contactpage.jsx
@@ -3,13 +3,13 @@ import { getDatabase, ref, push } from 'firebase/database';
 
 const firebaseConfig = {
     apiKey: process.env.REACT_APP_APIKEY,
-    authDomain: process.env.REACT_APP_AUTHDOMAIN ,
+    authDomain: process.env.REACT_APP_AUTHDOMAIN,
     projectId: process.env.REACT_APP_PROJECTID,
-    storageBucket: process.env.REACT_APP_STROAGEBLOCKED,
+    storageBucket: process.env.REACT_APP_STORAGEBUCKET,
     messagingSenderId: process.env.REACT_APP_MESSAGINGSENDERID,
     appId: process.env.REACT_APP_APPID,
     measurementId: process.env.REACT_APP_MEASUREMENTID,
-    databaseURL: process.env.REACT_APP_DATABASEURL
+    databaseURL: "https://codebloomwebsite-default-rtdb.firebaseio.com/"
 };
 
 const app = initializeApp(firebaseConfig);
@@ -23,15 +23,24 @@ const ContactPage = () => {
             name: name.value,
             email: email.value,
         };
-        push(ref(database, 'users'), data);
-    }
+        try {
+            push(ref(database, 'users'), data)
+                .then(() => {
+                    console.log("Data pushed successfully!");
+                })
+                .catch((error) => {
+                    console.error("Error pushing data:", error);
+                });
+        } catch (error) {
+            console.error("Error pushing data:", error);
+        }
+    };
+
     return (
         <>
             <h1>contact form</h1>
             <a href="/"><button>click me to visit index</button></a>
-
             <br></br>
-
             <form onSubmit={handleSubmit}>
                 <label>
                     Name:
@@ -44,6 +53,7 @@ const ContactPage = () => {
                 <button type="submit">Submit</button>
             </form>
         </>
-    )
-}
+    );
+};
+
 export default ContactPage;
